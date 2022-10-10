31eff453a630cf3a0ead4d022f23a31ac262cde7
Arthur Choi <aychoi@cs.ucla.edu>
2022-10-10 17:20:32 -0400

    fixed SPECT, added neuron directory

--- testing/SPECT/SPECT.py
+++ testing/SPECT/SPECT.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LogisticRegression
 
-data_dir = "/home/richard/School/research/SPECT_Heart_Data_Set/SPECT.train"
+data_dir = "SPECT.train"
 class_names=["OVERALL_DIAGNOSIS", "F1:", "F2:", "F3:", "F4:", "F5:", "F6:", "F7:", "F8:",
              "F9:", "F10:", "F11:", "F12:", "F13:", "F14:", "F15:", "F16:", "F17:", "F18:", 
              "F19:", "F20:", "F21:", "F22:"]
@@ -20,11 +20,12 @@ A = np.array(train_features)
 y = np.array(train_labels)
 w = np.array(model.coef_).T 
 b = np.array(model.intercept_) 
-sum((A@w >= -b).flatten() == y)/len(y)
+acc = sum((A@w >= -b).flatten() == y)/len(y)
+print("       my accuracy: %.8f%%" % (100*acc,))
 
 #creating file
 arr = model.coef_[0]
-file1 = open("/home/richard/School/research/nnf2sdd2/examples/SPECT.neuron","w")
+file1 = open("neuron/SPECT.neuron","w")
 file1.write("name: example")
 file1.write("\nsize: ")
 file1.write((str)(len(model.coef_[0])))
@@ -34,5 +35,5 @@ for x in range(len(arr)):
     file1.write(str(model.coef_[0][x]))
                     
 file1.write("\nthreshold: ")
-file1.write(str(model.intercept_[0]))
+file1.write(str(-model.intercept_[0]))
 
