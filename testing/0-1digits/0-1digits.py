5f01326684a7443135bdee944e7ec2115ebb7cb5
Arthur Choi <aychoi@cs.ucla.edu>
2022-11-05 17:09:22 -0400

    simplified the code a little, updated for integers

--- testing/0-1digits/0-1digits.py
+++ testing/0-1digits/0-1digits.py
@@ -14,7 +14,7 @@ train_features = dataset
 train_labels = train_features.pop("1.48")
 
 
-model = LogisticRegression(penalty='l1',solver='liblinear',C=.001) # tol=1e-8,
+model = LogisticRegression(penalty='l1',solver='liblinear',C=.002) #tol=1e-8
 classifier = model.fit(train_features,train_labels)
 train_accuracy = 100*classifier.score(train_features,train_labels)
 print("LogisticRegression: %.8f%% (training accuracy)" % (train_accuracy,))
@@ -27,24 +27,30 @@ b = np.array(model.intercept_)
 acc = sum((A@w > -b).flatten() == y)/len(y)
 print("       my accuracy: %.8f%%" % (100*acc,))
 
+print_integer = False
+# ACAC: if using integers, need to re-check accuracy
+
+digits = 4
+alpha = 10**digits
+
+def write_parameter(file1,parameter):
+    if print_integer:
+        parameter = alpha*parameter
+        file1.write("%d" % parameter)
+    else:
+        file1.write("%f" % parameter)
 
 #creating file
-arr = model.coef_[0]
 file1 = open("neuron/0-1digits.neuron","w")
 file1.write("name: example")
-file1.write("\nsize: ")
-file1.write((str)(len(model.coef_[0])))
+file1.write("\nsize: %d" % len(model.coef_[0]))
 file1.write("\nweights: ")
-for x in range(len(arr)):
+for parameter in model.coef_[0]:
     file1.write(" ")
-    file1.write("%f" % model.coef_[0][x])
-                    
+    write_parameter(file1,parameter)
 file1.write("\nthreshold: ")
-file1.write("%f" % -model.intercept_[0])
+write_parameter(file1,model.intercept_[0])
 file1.write("\n ")
 
-non_zero_count = 0
-for w in model.coef_[0]:
-    if w != 0:
-        non_zero_count += 1
+non_zero_count = sum(1 for w in model.coef_[0] if w != 0)
 print("non-zero: %d" % non_zero_count)
